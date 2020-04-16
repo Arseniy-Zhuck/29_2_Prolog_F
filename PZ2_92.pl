ab576ef1f4e27fadf429b701f8e808b710ead41d
Unknown <arseniyzhuck@mail.ru>
2020-04-16 12:08:12 +0300

    aky_start

--- PZ2_92.pl
+++ PZ2_92.pl
@@ -156,6 +156,69 @@ sub_set_k([H|Sub_set],[H|Set],K):- K1 is K-1,sub_set_k(Sub_set,Set,K1).
 sub_set_k(Sub_set,[H|Set],K):-sub_set_k(Sub_set,Set,K).
 pr_subset_k:-read_str(A,N),read(K),sub_set_k(B,A,K),write_str(B),nl,fail.
 
+pr7_7:- List[_,_,_,_],
+	( 
+	(	(in_list(List,[_,fantic,white]);in_list(List,[_,fantic,grey]);
+		in_list(List,[_,fantic,black])),
+		(in_list(List,[_,murl,white]);in_list(List,[_,murl,black]);
+		in_list(List,[_,murl,red])),
+		(in_list(List,[_,dru,black]);in_list(List,[_,dru,grey]);
+		in_list(List,[_,dru,red])),
+		(in_list(List,[_,elis,white]);in_list(List,[_,elis,black]);
+		in_list(List,[_,elis,red])),
+		in_list(List,[misha,_,black]),in_list(List,[maksim,murl,_]),
+		in_list(List,[leo,elis,_]),in_list(List,[dima,_,white]),
+		((in_list(List,[dima,fantic,_]));(in_list(List,[_,dru,grey])))
+	);
+	(	(in_list(List,[_,fantic,white]);in_list(List,[_,fantic,grey]);
+		in_list(List,[_,fantic,black])),
+		(in_list(List,[_,murl,white]);in_list(List,[_,murl,grey]);
+		in_list(List,[_,murl,red])),
+		(in_list(List,[_,dru,black]);in_list(List,[_,dru,grey]);
+		in_list(List,[_,dru,red])),
+		(in_list(List,[_,elis,white]);in_list(List,[_,elis,black]);
+		in_list(List,[_,elis,red])),
+		in_list(List,[misha,_,black]),in_list(List,[maksim,murl,_]),
+		(in_list(List,[dima,murl,_]);in_list(List,[dima,dru,_]);
+		in_list(List,[dima,elis,)]))
+	);
+	(
+		(in_list(List,[_,fantic,white]);in_list(List,[_,fantic,grey]);
+		in_list(List,[_,fantic,black])),
+		(in_list(List,[_,murl,white]);in_list(List,[_,murl,grey]);
+		in_list(List,[_,murl,red])),
+		(in_list(List,[_,dru,black]);in_list(List,[_,dru,grey]);
+		in_list(List,[_,dru,red])),
+		(in_list(List,[_,elis,white]);in_list(List,[_,elis,black]);
+		in_list(List,[_,elis,red])),
+		in_list(List,[leo,elis,_]),in_list(List,[dima,_,white]),
+		(in_list(List,[dima,murl,_]);in_list(List,[dima,dru,_]);
+		in_list(List,[dima,elis,)]))
+		
+	);
+	(
+		(in_list(List,[_,fantic,white]);in_list(List,[_,fantic,grey]);
+		in_list(List,[_,fantic,black])),
+		(in_list(List,[_,murl,white]);in_list(List,[_,murl,grey]);
+		in_list(List,[_,murl,red])),
+		in_list(List,[misha,_,black]),in_list(List,[maksim,murl,_]),
+		in_list(List,[leo,elis,_]),in_list(List,[dima,_,white]),
+		(in_list(List,[dima,murl,_]);in_list(List,[dima,dru,_]);
+		in_list(List,[dima,elis,)]))
+		
+	);
+	(
+		(in_list(List,[_,dru,black]);in_list(List,[_,dru,grey]);
+		in_list(List,[_,dru,red])),
+		(in_list(List,[_,elis,white]);in_list(List,[_,elis,black]);
+		in_list(List,[_,elis,red])),
+		in_list(List,[misha,_,black]),in_list(List,[maksim,murl,_]),
+		in_list(List,[leo,elis,_]),in_list(List,[dima,_,white]),
+		(in_list(List,[dima,murl,_]);in_list(List,[dima,dru,_]);
+		in_list(List,[dima,elis,)]))
+	)
+	),
+	write(List).
 
 /*unique(A, Result):- 
         unique(A, Result, []), !.
