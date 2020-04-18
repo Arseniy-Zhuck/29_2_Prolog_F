cfd58f19281fcd28f52491f89ffa429311cccc8d
Unknown <arseniyzhuck@mail.ru>
2020-04-18 11:22:51 +0300

    PZ9_Res

--- 1_3/1_3/Program.fs
+++ 1_3/1_3/Program.fs
@@ -2,17 +2,21 @@
 // See the 'F# Tutorial' project for more help.
 let sum x y = x+y
 let pr x y = x*y
+let sk x y =x*x+y*y
 let p f x y = f x y
 
 let rec vich f n =
-        match n with 
-        | 1 -> 1
-        | _ ->  let v = vich f (n-1) in
-                p f n v 
+    if n=1 then 1
+    else    let v = vich f (n-1) 
+            p f n v
+//        match n with 
+//        | 1 -> 1
+//        | _ ->  let v = vich f (n-1) in
+//                p f n v 
 
 let p9_3 =
     let b = System.Convert.ToInt32(System.Console.ReadLine())
-    (vich sum b,vich pr b)
+    (vich sum b,vich pr b, vich sk b)
 
 
 [<EntryPoint>]
