cb666808c87e47fc15f3570954054c6a9ed0aafd
Unknown <arseniyzhuck@mail.ru>
2020-04-18 10:12:04 +0300

    Квадратное уравнение

--- PZ1. max/PZ1. max/Program.fs
+++ PZ1. max/PZ1. max/Program.fs
@@ -1,27 +1,18 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
+let max2 x y =
+    if x>y then x
+    else y
 
+let p1_1 = 
+    let b = System.Convert.ToInt32(System.Console.ReadLine())
+    let c = System.Convert.ToInt32(System.Console.ReadLine())
+    max2 b c
 
 
 [<EntryPoint>]
 let main argv =
-    
-    let b = System.Convert.ToInt32(System.Console.ReadLine())
-    let c = System.Convert.ToInt32(System.Console.ReadLine())
-    let p1_1 x y =
-        if x>y then x
-        else y 
-    let A = p1_1 b c
-    System.Console.Write(A)
-    
-    let rec f n =
-        if n=0 then 1
-        else 
-            let n1=n-1
-            f n1
-    let f1= f 10
-    printfn "%i" f1
+    let Ans = p1_1
+    System.Console.Write(Ans)
     let z = System.Console.ReadKey()
-     
-    
     0 // return an integer exit code
