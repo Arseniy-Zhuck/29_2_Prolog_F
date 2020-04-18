7c4a2efd81aaaee3423db4828575e79f858159a0
Unknown <arseniyzhuck@mail.ru>
2020-04-18 10:56:15 +0300

    1_1, 1_2, 1_3

---
+++ 1_3/1_3/Program.fs
@@ -0,0 +1,26 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+let sum x y = x+y
+let pr x y = x*y
+let p f x y = f x y
+
+let rec vich f n =
+        match n with 
+        | 1 -> 1
+        | _ ->  let v = vich f (n-1) in
+                p f n v 
+
+let p9_3 =
+    let b = System.Convert.ToInt32(System.Console.ReadLine())
+    (vich sum b,vich pr b)
+
+
+[<EntryPoint>]
+let main argv = 
+    
+    
+    let Ans = p9_3
+    System.Console.Write(Ans)
+    let z = System.Console.ReadKey()
+    
+    0 // return an integer exit code
--- ConsoleApplication6/ConsoleApplication6/Program.fs
+++ ConsoleApplication6/ConsoleApplication6/Program.fs
@@ -10,7 +10,7 @@ let main argv =
     let solve a b c =
         let D = b*b - 4.0*a*c
         ((-b-sqrt(D))/(2.0*a),(-b+sqrt(D))/(2.0*a))
-    printfn "%A%A" solve (1.0, 2.0, -3.0)
+    printfn "%A%A" (solve 1.0 2.0 (-3.0))
     
     let x = y 10 in    
         printfn "%i" x
